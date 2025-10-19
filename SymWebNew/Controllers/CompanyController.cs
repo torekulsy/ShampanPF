269067e347ac5b78fcce9eb396d59e882b9a10c1
DEV-1007\Torekul <torekul.islam@symphonysoftt.com>
2025-10-19 14:05:06 +0600

    correction

--- SymWebNew/Controllers/CompanyController.cs
+++ SymWebNew/Controllers/CompanyController.cs
@@ -1,7 +1,9 @@
-﻿using SymRepository.Common;
+﻿using SymOrdinary;
+using SymRepository.Common;
 using SymViewModel.Common;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,7 +16,8 @@ namespace SymWebUI.Controllers
         // GET: /Company/
         BranchRepo branchRepo = new BranchRepo();
         ShampanIdentityVM vm = new ShampanIdentityVM();
-
+       
+        
         public ActionResult Index()
         {
             List<BranchVM> company = branchRepo.SelectAll();
@@ -24,6 +27,10 @@ namespace SymWebUI.Controllers
         {
             Session["BranchId"] = Id;
             vm.BranchId = Id;
+
+            BranchRepo BranchRepo = new BranchRepo();
+            BranchVM branch = BranchRepo.SelectById(Convert.ToInt32(vm.BranchId));
+            Session["BranchName"] = branch.Name;
 
             return Redirect("/Common/Home");
         }
