4cd582faec3873d251bcacb0787d5170f232d28a
DEV-1008\Perves <pipul.rahman@symphonysoftt.com>
2025-11-26 17:05:54 +0600

    COA sub group & category

--- SymWebNew/Areas/Config/Controllers/DropDownController.cs
+++ SymWebNew/Areas/Config/Controllers/DropDownController.cs
@@ -56,6 +56,11 @@ namespace SymWebUI.Areas.Config.Controllers
             return Json(new SelectList(new COAGroupRepo().DropDown(TransType), "Id", "Name"), JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult PF_COASubGroup()
+        {
+            return Json(new SelectList(new COASubGroupRepo().DropDown(), "Id", "Name"), JsonRequestBehavior.AllowGet);
+        }
+
         public JsonResult PF_COATypeOfReport(string TransType = "PF")
         {
             return Json(new SelectList(new COAGroupRepo().COATypeOfReportDropDown(TransType), "Id", "Name"), JsonRequestBehavior.AllowGet);
